bd5e686f5f29e3309181b48d044e368e0d3e1204
Matthias Schöffel <m.schoeffel@reply.de>
2022-05-20 16:35:53 +0200

    Implement dummy LTI System

--- DD_DE/lti_system.py
+++ DD_DE/lti_system.py
@@ -1,2 +1,13 @@
-def test():
-    print("das ist LTI-SYSTEM!")
+import numpy as np
+
+
+class LTISystem:
+    # Todo: Insert disturbance
+    def __init__(self, x, A, B):
+        self.A = np.array(A, dtype=float)
+        self.B = np.array(B, dtype=float)
+        self.x = np.array(x, dtype=float)
+
+    def next_step(self, u):
+        self.u = np.array(u)
+        self.x = self.A @ self.x + self.B @ self.u
--- DD_DE/main.py
+++ DD_DE/main.py
@@ -1,8 +1,18 @@
 from DD_DE import lti_system
+from DD_DE import disturbance_estimator
+from DD_DE import data_driven_predictor
+
 
 def main():
     print("Hello World!")
-    lti_system.test()
+    my_system = lti_system.LTISystem([1,0], [[2,0],[1,1]], [[1,0],[0,1]])
+    print(my_system.x)
+    my_system.next_step([2,2])
+    print(my_system.x)
+    my_system.next_step([3,3])
+    print(my_system.x)
+
+
 
 if __name__ == "__main__":
     main()
