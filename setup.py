eb702678bd03d19b83e65c4d285ec60cf88e1879
Matthias Schöffel <m.schoeffel@reply.de>
2022-05-20 15:00:45 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name="data_driven_disturbance_estimator",
+    version="0.0.1",
+    author="Matthias Schöffel",
+    author_email="matthiasschoeffel97@gmail.com",
+    description=(
+        "Uses data driven system representation and kernel density estimator to estimate disturbance on LTI-system."),
+    license="BSD",
+    keywords="data driven disturbance estimator",
+    url="https://github.com/ratherbeflyin2080/data_driven_disturbance_estimator",
+    packages=['DD_DE', 'tests'],
+)
