7494154263c96d52f064f5245d7a698c9156aa94
Matthias Schöffel <m.schoeffel@reply.de>
2022-05-27 14:42:46 +0200

    Fix data driven predictor
    Pseudo-Inverse was wrong
    
    Calculate pseudo inverse by only taking "fixed values" in hankel matrix

--- DD_DE/data_driven_predictor.py
+++ DD_DE/data_driven_predictor.py
@@ -4,17 +4,18 @@ import numpy as np
 class DDPredictor:
     def __init__(self, h_matrix):
         self.h_matrix = np.array(h_matrix)
-        self.h_matrix_inv = np.linalg.pinv(self.h_matrix)
+        print(f"h_matrix (numpy) \n {self.h_matrix}")
+
+        h_input_state = self.h_matrix[[0,2],:]
+        print(f"h_input_state (numpy) \n {h_input_state}")
+        self.h_matrix_inv = np.linalg.pinv(h_input_state)
 
     # Todo: Currently for one dimensional state space, has to be changed for multidimensional state space
 
     def predict_state(self,current_x,u):
-        goal_vector = np.transpose(np.array([u,0,current_x,0]))
-        print(f"goal vector: {goal_vector}")
+        goal_vector = np.transpose(np.array([u,current_x]))
         alpha = self.h_matrix_inv @ goal_vector
-        print(f"alpha: {alpha}")
         # Todo: Change name prediction (bad name)
         prediction = self.h_matrix @ alpha
-        print(f"Prediction: {prediction}")
         next_x = prediction[3]
         return next_x
--- DD_DE/main.py
+++ DD_DE/main.py
@@ -10,25 +10,27 @@ def get_dist() -> float:
 
 
 def main():
-    my_system = lti_system.LTISystem(0, 1, 1, get_dist)
+    my_system = lti_system.LTISystem(x=0, A=1, B=1, get_dist=get_dist)
 
-    h_matrix = [[1,-1,0,-1],[0,1,1,0],[0,0,1,1],[1,-1,1,0]]
+    h_matrix = [[1, -1, 0, -1], [0, 1, 1, 0], [0, 0, 1, 1], [1, -1, 1, 0]]
     my_predictor = data_driven_predictor.DDPredictor(h_matrix)
 
     print(my_predictor.h_matrix)
     print(my_predictor.h_matrix_inv)
 
-    state_prediction = my_predictor.predict_state(my_system.x,1)
-    print(state_prediction)
+    print(f"actual state: {my_system.x}")
 
-    # print(f"actual state: {my_system.x}")
+    print("\n\n1:")
 
-    # print(f"Predicted state:  {my_predictor.predict_state(my_system.x,1)}")
-    # my_system.next_step(1)
-    # print(f"actual state: {my_system.x}")
+    print(f"Predicted state:  {my_predictor.predict_state(my_system.x,1)}")
+    my_system.next_step(1)
+    print(f"actual state: {my_system.x}")
 
-    # my_system.next_step(2)
-    # print(f"actual state: {my_system.x}")
+    print("\n\n2:")
+
+    print(f"Predicted state:  {my_predictor.predict_state(my_system.x,2)}")
+    my_system.next_step(2)
+    print(f"actual state: {my_system.x}")
 
 
 if __name__ == "__main__":
