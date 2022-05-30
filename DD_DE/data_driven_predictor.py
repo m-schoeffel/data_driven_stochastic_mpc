dafb748f5c6ca80633579975f7ba4f804379bffe
Matthias Schöffel <m.schoeffel@reply.de>
2022-05-30 17:51:25 +0200

    Minor clean up and integrate numpy in gaussian_process module

--- DD_DE/data_driven_predictor.py
+++ DD_DE/data_driven_predictor.py
@@ -4,10 +4,10 @@ import numpy as np
 class DDPredictor:
     def __init__(self, h_matrix):
         self.h_matrix = np.array(h_matrix)
-        print(f"h_matrix (numpy) \n {self.h_matrix}")
+        # print(f"h_matrix (numpy) \n {self.h_matrix}")
 
         h_input_state = self.h_matrix[[0,2],:]
-        print(f"h_input_state (numpy) \n {h_input_state}")
+        # print(f"h_input_state (numpy) \n {h_input_state}")
         self.h_matrix_inv = np.linalg.pinv(h_input_state)
 
     # Todo: Currently for one dimensional state space, has to be changed for multidimensional state space
--- disturbance_estimator/gaussian_process.py
+++ disturbance_estimator/gaussian_process.py
@@ -1,14 +1,29 @@
+import numpy as np
+
+# from sklearn.gaussian_process import GaussianProcessRegressor
+# from sklearn.gaussian_process.kernels import RBF
+
+
 class GaussianProcess:
     def __init__(self):
 
-        # Store tuples of index k and delta x (difference between prediction and actual state)
-        self.list_delta_x = list()
+        # Store index k and delta x (difference between prediction and actual state)
+        self.array_delta_x = np.ones((100,2))
+        self.numbr_measurements=0
 
-    def add_delta_x(self,index_k,delta_x):
-        self.list_delta_x.append((index_k,delta_x))
+    def add_delta_x(self, index_k, delta_x):
+        self.array_delta_x[self.numbr_measurements,:] = ([index_k, delta_x])
+        self.numbr_measurements += 1
 
     def plot_distribution(self):
         # Todo: Distribution should be plottet here
         # Right now self.delta will be displayed
 
-        print(self.list_delta_x)
+        # print(self.list_delta_x)
+        print(self.array_delta_x[0:self.numbr_measurements, 0])
+        print(self.array_delta_x[0:self.numbr_measurements+1, 1])
+
+        # kernel = 1 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2))
+        # gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+        # gaussian_process.fit(self.list_delta_x[0,:], self.list_delta_x[1,:])
+        # gaussian_process.kernel_
