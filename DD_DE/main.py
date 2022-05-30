dae6fdf9545f180394fdaf7873bfd247d1cb5294
Matthias Schöffel <m.schoeffel@reply.de>
2022-05-30 16:48:41 +0200

    Write base version of disturbance estimators

--- DD_DE/main.py
+++ DD_DE/main.py
@@ -16,17 +16,25 @@ def main():
     h_matrix = [[1, -1, 0, -1], [0, 1, 1, 0], [0, 0, 1, 1], [1, -1, 1, 0]]
     my_predictor = data_driven_predictor.DDPredictor(h_matrix)
 
-    print(my_predictor.h_matrix)
-    print(my_predictor.h_matrix_inv)
+    disturbance_estimator = gaussian_process.GaussianProcess()
 
     print(f"actual state: {my_system.x}")
 
     for u in [1,0,-3,4,-5,6]:
-        print(f"\n\n{my_system.k}:")
+        print(f"\n\nk = {my_system.k}:")
+
+        predicted_state = my_predictor.predict_state(my_system.x,u)
 
         print(f"Predicted state:  {my_predictor.predict_state(my_system.x,u)}")
         my_system.next_step(u)
         print(f"actual state: {my_system.x}")
+
+        delta_x = my_system.x - predicted_state
+
+        disturbance_estimator.add_delta_x(my_system.k,delta_x)
+
+    disturbance_estimator.plot_distribution()
+
 
 
 if __name__ == "__main__":
---
+++ disturbance_estimator/traditional_kernel_density_estimator.py
@@ -0,0 +1,14 @@
+class TraditionalKDE:
+    def __init__(self):
+
+        # Store tuples of index k and delta x (difference between prediction and actual state)
+        self.list_delta_x = list()
+
+    def add_delta_x(self,index_k,delta_x):
+        self.list_delta_x.append((index_k,delta_x))
+
+    def plot_distribution(self):
+        # Todo: Distribution should be plottet here
+        # Right now self.delta will be displayed
+
+        print(self.list_delta_x)
