3339cae55792e574ed0d81a9e3c1c43bd20c63de
Matthias Schöffel <m.schoeffel@reply.de>
2022-07-07 21:28:09 +0200

    Outsource creating hankel matrix to helpers.py

--- DD_DE/data_driven_mpc.py
+++ DD_DE/data_driven_mpc.py
@@ -1,42 +1,14 @@
 import numpy as np
 
+from DD_DE import helpers
+
 
 class DataDrivenMPC:
     def __init__(self, input_sequence, state_sequence, prediction_horizon):
-        n = prediction_horizon
 
-        self.dim_u = input_sequence.shape[0]
-        self.dim_x = state_sequence.shape[0]
+        self.h_matrix = helpers.create_hankel_matrix(input_sequence,state_sequence,prediction_horizon)
 
-        hankel_u_shape = (
-            input_sequence.shape[0]*(n+1), input_sequence.shape[1]-n)
-        hankel_x_shape = (
-            state_sequence.shape[0]*(n+1), input_sequence.shape[1]-n)
-
-        hankel_u = np.zeros(hankel_u_shape)
-        hankel_x = np.zeros(hankel_x_shape)
-
-        # print(f"input_sequence:\n {input_sequence}")
-        # print(f"state_sequence:\n {state_sequence}")
-
-        # Split input and state sequences horizontally to efficiently concatenate them for varying prediction horizons
-        input_sequence_splitted = np.hsplit(
-            input_sequence, input_sequence.shape[1])
-        state_sequence_splitted = np.hsplit(
-            state_sequence, state_sequence.shape[1])
-
-        for i in range(0, input_sequence.shape[1]-n):
-            hankel_u[:, i] = np.concatenate(
-                tuple(input_sequence_splitted[idx] for idx in range(i, i+n+1)))[:,0]
-            hankel_x[:, i] = np.concatenate(
-                tuple(state_sequence_splitted[idx] for idx in range(i, i+n+1)))[:,0]
-        
-        # print(f"\nhankel_u: \n{hankel_u}")
-        # print(f"\nhankel_x: \n{hankel_x}")
-
-        self.h_matrix = np.concatenate((hankel_u, hankel_x))
-
-        print(self.h_matrix.shape)
+        print(self.h_matrix)
 
 
 input = np.array([[1, -1, 0, 2, 3, -4, 0, -6, 2],
--- DD_DE/helpers.py
+++ DD_DE/helpers.py
@@ -15,7 +15,7 @@ def load_parameters():
     # If dimension of input would be one, input sequence would be one dimensional array otherwise (breaks system)
     input_seq_len = len(param["input_sequence"])
     input_seq_dim = len(param["input_sequence"][0])
-    main_param["input_seq"] = np.zeros((input_seq_len,input_seq_dim))
+    main_param["input_seq"] = np.zeros((input_seq_len, input_seq_dim))
     main_param["input_seq"][:] = np.array(param["input_sequence"])
 
     main_param["number_of_measurements"] = param["number_of_measurements"]
@@ -32,3 +32,35 @@ def load_parameters():
     disc_kde_param["samples_considered"] = param["discounted_kde"]["number_of_past_samples_considered"]
 
     return main_param, lti_system_param, disc_kde_param
+
+
+def create_hankel_matrix(input_sequence, state_sequence, prediction_horizon):
+    n = prediction_horizon
+
+    dim_u = input_sequence.shape[0]
+    dim_x = state_sequence.shape[0]
+
+    length_hankel_matrix = input_sequence.shape[1]-n
+
+    hankel_u_shape = (
+        dim_u*(n+1), length_hankel_matrix)
+    hankel_x_shape = (
+        dim_x*(n+1), length_hankel_matrix)
+        
+    hankel_u = np.zeros(hankel_u_shape)
+    hankel_x = np.zeros(hankel_x_shape)
+
+    # Split input and state sequences horizontally to efficiently concatenate them for varying prediction horizons
+    input_sequence_splitted = np.hsplit(
+        input_sequence, input_sequence.shape[1])
+    state_sequence_splitted = np.hsplit(
+        state_sequence, state_sequence.shape[1])
+
+    for i in range(0, length_hankel_matrix):
+        hankel_u[:, i] = np.concatenate(
+            tuple(input_sequence_splitted[idx] for idx in range(i, i+n+1)))[:, 0]
+        hankel_x[:, i] = np.concatenate(
+            tuple(state_sequence_splitted[idx] for idx in range(i, i+n+1)))[:, 0]
+
+        h_matrix = np.concatenate((hankel_u, hankel_x))
+        return h_matrix
