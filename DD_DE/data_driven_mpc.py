641729e44acedccc514cbac0f365d22e49926993
Matthias Schöffel <m.schoeffel@reply.de>
2022-07-13 18:34:05 +0200

    Implement first version of cost function

--- DD_DE/data_driven_mpc.py
+++ DD_DE/data_driven_mpc.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from scipy.optimize import minimize, LinearConstraint
+
 from DD_DE import helpers
 from DD_DE import lti_system
 from DD_DE import data_driven_predictor
@@ -15,16 +17,15 @@ class DataDrivenMPC:
 
         # Todo: Load constraints from config.yaml
         # Hardcode constraints (for system with 2 inputs) for now
-        G_u = np.diag(np.ones(2))
-        G_u = np.concatenate([G_u, G_u])
-        g_u = np.array([10, 10, -10, -10]).reshape(-1, 1)
+        self.G_u = np.diag(np.ones(6))
+        self.g_u = np.array([1, 1, 1, 1, 1, 1])
 
         # Todo: Add constraints for states
         # Left out for now, because they have to be formulated in relation to u (extensive)
 
         # Matrices for input and state cost
-        R = np.diag(np.ones(2))
-        Q = np.diag(np.ones(2))
+        self.R = np.diag(np.ones(2))
+        self.Q = np.diag(np.ones(2))
 
         self.h_matrix = helpers.create_hankel_matrix(
             input_sequence, state_sequence, prediction_horizon)
@@ -33,7 +34,9 @@ class DataDrivenMPC:
 
         u_sequence = np.array([1, 2, -1, -2, 3, 5])
         current_x = np.array([0, 1])
-        self.predict_state_sequence(current_x, u_sequence)
+        trajectory=self.predict_state_sequence(current_x, u_sequence)
+        cost = self.get_sequence_cost(u_sequence,current_x)
+        print(f"The sum of traj is {trajectory.transpose()@trajectory} and the cost is {cost}")
 
     def get_new_u(self):
         # Todo: Später wirst du hier einen Zielstate als Input übergeben
@@ -52,6 +55,20 @@ class DataDrivenMPC:
         # 1. Funktion, die Us annimmt, und x_1, x_2, usw. zurückgibt
         # 2. Funktion, die quadrierte Variablen aufspannt für Optimierungsfunktion
         # 3. Funktion, die die Optimierungsfunktion anhand der Matrizen als Einzeiler zurückgibt
+
+        # Create U Constraints
+        constraint = LinearConstraint(
+            self.G_u, lb=-self.g_u, ub=self.g_u)
+
+    def get_sequence_cost(self, u_seq, current_x):
+        trajectory = self.predict_state_sequence(current_x, u_seq)
+        cost = 0
+        for i in [0, 2, 4]:
+            cost += trajectory[i:i+2].transpose()@self.Q@trajectory[i:i+2]
+
+        for i in [8, 10, 12, 14]:
+            cost += trajectory[i:i+2].transpose()@self.R@trajectory[i:i+2]
+        return cost
 
     def get_sum_states(self, u_sequence):
         print(f"\nget_sum_states:\n {u_sequence[0]}\n")
@@ -73,6 +90,7 @@ class DataDrivenMPC:
         # return next_x
         print(f"\n\ntrajectory: {np.round(trajectory).astype(np.int)}\n\n")
         # print(f"\n\ntrajectory: {trajectory}\n\n")
+        return trajectory
 
 
 # Testbench:
