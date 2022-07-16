97159c9129d41fd7a11a17e97f028e78c62769a3
Matthias Schöffel <m.schoeffel@reply.de>
2022-07-16 18:07:15 +0200

    Plot correct gaussian process

--- disturbance_estimator/gaussian_process.py
+++ disturbance_estimator/gaussian_process.py
@@ -10,12 +10,13 @@ from DD_DE import helpers
 class GaussianProcess:
     def __init__(self, number_of_states, number_timesteps):
 
-        self.number_of_past_samples_considered_for_kde = min(
+        self.number_of_past_samples_considered = min(
             number_timesteps, helpers.load_param_gaussian_process())
 
         self.number_of_states = number_of_states
 
         # Store index k and delta x of every state (difference between prediction and actual state)
+        # Todo: self.k_array currently not used
         self.k_array = np.zeros((1, number_timesteps), dtype=int)
         self.delta_x_array = np.zeros((number_of_states, number_timesteps))
         self.numbr_measurements = 0
@@ -30,10 +31,13 @@ class GaussianProcess:
 
         fig.suptitle("Gaussian process of each state")
 
+        idx_considered_for_estimation = list(range(self.numbr_measurements-self.number_of_past_samples_considered,self.numbr_measurements))
+        print(f"{idx_considered_for_estimation=}")
+
         for i in range(0, self.number_of_states):
             # A gaussian process has to be plotted for every state
-            X_train = self.delta_x_array[i,0:self.numbr_measurements].reshape(-1, 1)
-            y_train = self.k_array[0,0:self.numbr_measurements]
+            y_train = self.delta_x_array[i,idx_considered_for_estimation].reshape(-1, 1)
+            X_train = np.atleast_2d(list(range(0,self.number_of_past_samples_considered))).reshape(-1,1)
 
             kernel = 1 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2))
             gaussian_process = GaussianProcessRegressor(
@@ -42,7 +46,7 @@ class GaussianProcess:
             print(gaussian_process.kernel_)
 
             X_predict = np.linspace(
-                1, len(X_train)+1, self.numbr_measurements*2).reshape(-1, 1)
+                0, self.number_of_past_samples_considered, self.number_of_past_samples_considered*20).reshape(-1, 1)
 
             mean_prediction, std_prediction = gaussian_process.predict(
                 X_predict, return_std=True)
