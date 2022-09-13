df6b790fe46125b2384decf41ab874c9be22d304
Matthias Schöffel <matthiasschoeffel97@gmail.com>
2022-09-13 19:20:12 +0200

    Clean Up main loop

--- simulation/main.py
+++ simulation/main.py
@@ -26,27 +26,18 @@ def main():
 
     state_storage = np.zeros([x_initial_state.shape[0],number_of_measurements])
     
-    print(f"ms1: {number_of_measurements}")
     for i in range(0, number_of_measurements):
         start_time = time.time()
-        # print(f"\n\nk = {real_system.k}:")
 
-        # Todo: Nächste Zeile muss mit MPC ausgetauscht werden
-        u = np.random.randint(-5,5,size=(1,2))
         next_u = dd_mpc.get_new_u(real_system.x,goal_state=[-2,-2,0,0])
-        # print(next_u)
-
         predicted_state = dd_predictor.predict_state(real_system.x, next_u)
-
-        # print(f"Predicted state:  {predicted_state}")
         real_system.next_step(next_u,add_disturbance=False)
-        # print(f"actual state: \n{real_system.x}")
-
-        delta_x = real_system.x - predicted_state
 
         state_storage[:,i]=real_system.x.reshape(-1)
 
+        delta_x = real_system.x - predicted_state
         disturbance_estimator.add_delta_x(real_system.k, delta_x)
+
         print("--- \"Main Loop\" took %s seconds ---" % (time.time() - start_time))
     
 
