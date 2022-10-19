54d755c10408b31f0f225349415f336cffa30c71
Matthias Schöffel <matthiasschoeffel97@gmail.com>
2022-10-19 14:29:20 +0200

    Delete deprecated code

--- simulation/main.py
+++ simulation/main.py
@@ -49,9 +49,6 @@ def main():
         multivariate_kde = disturbance_estimator.get_kde_multivariate_dist()
         [G_v, g_v, G_z, g_z] = constraint_tightener.tighten_constraints_on_multivariate_kde(multivariate_kde)
 
-        # dist_intervals = disturbance_estimator.get_disturbance_intervals()
-        # [G_v, g_v, G_z, g_z] = constraint_tightener.tighten_constraints_on_interv(dist_intervals)
-
         ref_pred_hor = ref_traj[:, i:i+prediction_horizon_size]
         [next_u, x_pred, prediction_horizon] = dd_mpc.get_new_u(
             real_system.x, G_v, g_v, G_z, g_z, ref_pred_hor=ref_pred_hor)
