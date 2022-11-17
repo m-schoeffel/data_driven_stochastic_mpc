04ad31495a80c655228640bb739bd67c4e67edc4
Matthias Schöffel <matthiasschoeffel97@gmail.com>
2022-11-17 21:30:17 +0100

    Evaluate KDE on less runs

--- figures_thesis/eval_kde/eval_kde.py
+++ figures_thesis/eval_kde/eval_kde.py
@@ -11,11 +11,11 @@ from matplotlib import pyplot as plt
 mean=0
 std_dev=1
 
-x_eval = np.linspace(-5, 5, 10001)
+x_eval = np.linspace(-4, 4, 4001)
 
 real_distr = 1/(std_dev*np.sqrt(2*np.pi))*np.exp(-0.5*np.power((x_eval-mean)/std_dev,2))
 
-# test_s = np.random.normal(loc=mean, scale=std_dev,size=5)
+# test_s = np.random.normal(loc=mean, scale=std_dev,size=1000)
 # kde = stats.gaussian_kde(test_s)
 
 # test_d = kde.evaluate(x_eval)
@@ -27,7 +27,7 @@ real_distr = 1/(std_dev*np.sqrt(2*np.pi))*np.exp(-0.5*np.power((x_eval-mean)/std
 # plt.show()
 
 max_num_samp = 1000
-num_runs = 1000
+num_runs = 100
 
 square_error_storage = list()
 
