17385f3bcee4821671a08550da6f70e4bb53964b
Matthias Schöffel <matthiasschoeffel97@gmail.com>
2022-12-06 16:50:06 +0100

    Create new disturbance sequence for evaluation case 5

--- lti_system/disturbance_sequences/eval_7/eval_7.py
+++ lti_system/disturbance_sequences/eval_7/eval_7.py
@@ -9,12 +9,7 @@ import numpy as np
 
 name_sequence = "eval_7"
 
-x_1 = np.random.normal(loc=0, scale=0.005,size=500)
-x_2 = np.random.normal(loc=0, scale=0.005,size=500)
-x_3 = np.random.normal(loc=0, scale=0.005,size=500)
-x_4 = np.random.normal(loc=0, scale=0.005,size=500)
-
-compl_dist = np.vstack((x_1,x_2,x_3,x_4))
+compl_dist = np.zeros([4,500])
 
 path_dist = os.path.join(os.getcwd(),"lti_system","disturbance_sequences",name_sequence,name_sequence)
 np.save(path_dist,compl_dist)
