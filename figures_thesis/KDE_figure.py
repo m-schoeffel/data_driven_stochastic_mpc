82c0bea0688502e5e3887b1d41edb9f53e00405d
Matthias Schöffel <matthiasschoeffel97@gmail.com>
2023-01-01 22:08:57 +0100

    Create figures to showcase kde

--- figures_thesis/KDE_figure.py
+++ figures_thesis/KDE_figure.py
@@ -1,33 +1,34 @@
+import os
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
 
-# Matplot config
-matplotlib.rcParams['lines.linewidth'] = 3
-matplotlib.rcParams['lines.markersize'] = 10
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-
-plt.rc('font', size=12)  # controls default text size
-# plt.rc('axes', titlesize=10) #fontsize of the title
-# plt.rc('axes', labelsize=10) #fontsize of the x and y labels
-# plt.rc('xtick', labelsize=10) #fontsize of the x tick labels
-# plt.rc('ytick', labelsize=10) #fontsize of the y tick labels
-plt.rc('legend', fontsize=10)  # fontsize of the legend
 
 cm = 1/2.54  # centimeters in inches
-my_figsize = (16*cm, 8*cm)
+
+plt.rc('font', size=8)
+# plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+without_serif= font_manager.FontProperties(family='sans-serif',
+                                   style='normal', size=7.5)
+
+cm = 1/2.54  # centimeters in inches
+my_figsize = (7*cm, 4*cm)
+
 fig = plt.figure(figsize=my_figsize)
+fig.tight_layout()
 
 
 data_points = np.array([-6, 2, 0, 3, 7])
 y_data_points = np.zeros(len(data_points))
 
 
-x = np.linspace(-10, 10, 10000)
+x = np.linspace(-13, 13, 10000)
 
 # Bandwith
-h = 2
+h = 4
 
 
 def gauss_kernel(x, x_i):
@@ -49,22 +50,24 @@ for i, point in enumerate(data_points):
 
 y_kernel_functions = y_kernel_functions/len(data_points)
 
-# for i, point in enumerate(y_kernel_functions):
+# # for i, point in enumerate(y_kernel_functions):
 
-plt.title(
-    r"\TeX\ is Number "r"$\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!")
+# plt.title(
+#     r"\TeX\ is Number "r"$\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!")
 
-plt.plot(x, y_kernel_functions, label="Kernel functions")
+plt.plot(x, y_kernel_functions, label="Kernel functions",linestyle='--',color="blue", lw=0.8)
 
-plt.plot(x, np.sum(y_kernel_functions, 1), linestyle='--', label="hallo")
+plt.plot(x, np.sum(y_kernel_functions, 1), color="red", lw=0.8)
 
 plt.scatter(data_points, y_data_points, marker='x',
-            linewidths=15, label="Datapoints")
+            linewidths=2, color="green", label="Datapoints")
 
-plt.xlabel("This is my x.")
+plt.xlabel("$s$",usetex=True)
+plt.ylabel("$f(s)$",usetex=True)
 
-plt.legend(loc='upper left')
+# plt.legend(loc='upper left')
 
-plt.savefig("my_first_saved_plot.pdf", format="pdf", bbox_inches="tight")
+path_dataset = os.path.join(os.getcwd(),"figures_thesis","kde_examples","kde_example_h_"+str(h)+".pdf")
+fig.savefig(path_dataset, format="pdf", bbox_inches="tight")
 
-plt.show()
+# plt.show()
