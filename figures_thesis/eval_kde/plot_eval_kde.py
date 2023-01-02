acd510604c96f45ead9a364779d620b366461d06
Matthias Schöffel <matthiasschoeffel97@gmail.com>
2023-01-02 17:01:01 +0100

    Apply formatting

--- figures_thesis/eval_kde/plot_eval_kde.py
+++ figures_thesis/eval_kde/plot_eval_kde.py
@@ -5,52 +5,56 @@ from matplotlib import pyplot as plt
 import matplotlib.font_manager as font_manager
 
 
-mean=0
-std_dev=1
-num_runs=1000
-num_max_samples=1000
+mean = 0
+std_dev = 1
+num_runs = 1000
+num_max_samples = 1000
 
 plt.rc('font', size=8)
 # plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
-without_serif= font_manager.FontProperties(family='sans-serif',
-                               style='normal', size=7.5)
+without_serif = font_manager.FontProperties(family='sans-serif',
+                                            style='normal', size=7.5)
 cm = 1/2.54  # centimeters in inches
 my_figsize = (15*cm, 8*cm)
 fig = plt.figure(figsize=my_figsize)
 
-path_store_animation = os.path.join(os.getcwd(),"figures_thesis","eval_kde","eval_kde_gaussian_mean_"+str(mean)+"_std_"+str(std_dev)+"_runs_"+str(num_runs)+".npy")
+path_store_animation = os.path.join(os.getcwd(), "figures_thesis", "eval_kde",
+                                    "eval_kde_gaussian_mean_"+str(mean)+"_std_"+str(std_dev)+"_runs_"+str(num_runs)+".npy")
 square_error_storage = np.load(path_store_animation)
 
 # pdf was not normalized before recording squared error
 # is fixed here
 error_storage = np.sqrt(square_error_storage)
 norm_error_storage = error_storage * (4-(-4))/4001
-square_error_storage = np.power(norm_error_storage,2)
+square_error_storage = np.power(norm_error_storage, 2)
 
-x=list(range(6,num_max_samples+1))
+x = list(range(6, num_max_samples+1))
 
 plt.yscale("log")
 
-mean_se = np.sum(square_error_storage[:,5::],0)/num_runs
-plt.plot(x,mean_se,label="Mean square error $\mu$")
+mean_se = np.sum(square_error_storage[:, 5::], 0)/num_runs
+plt.plot(x, mean_se, label="Mean square error $\mu$")
 
-std_dev_se = np.std(square_error_storage[:,5::],axis=0)
-plt.fill_between(x, mean_se-std_dev_se, mean_se+std_dev_se,color="orange",alpha=0.2)
+std_dev_se = np.std(square_error_storage[:, 5::], axis=0)
+plt.fill_between(x, mean_se-std_dev_se, mean_se +
+                 std_dev_se, color="orange", alpha=0.2)
 
-plt.plot(x,mean_se-std_dev_se,color="orange",label="First standard deviation $\sigma$")
-plt.plot(x,mean_se+std_dev_se,color="orange")
+plt.plot(x, mean_se-std_dev_se, color="orange",
+         label="First standard deviation $\sigma$")
+plt.plot(x, mean_se+std_dev_se, color="orange")
 
-plt.xlabel("Number of samples $o$ used for KDE",usetex=True)
-plt.ylabel("Square error $\epsilon_{\mathrm{square}}$",usetex=True)
+plt.xlabel("Number of samples $o$ used for KDE", usetex=True)
+plt.ylabel("Square error $\epsilon_{\mathrm{square}}$", usetex=True)
 
 print(std_dev_se[0:10])
 
-plt.legend(loc="upper right",prop=without_serif)
+plt.legend(loc="upper right", prop=without_serif)
 
 plt.tight_layout()
 
-path_cur_plot = os.path.join(os.getcwd(),"figures_thesis","eval_kde","eval_kde_runs_"+str(num_runs)+".pdf")
+path_cur_plot = os.path.join(
+    os.getcwd(), "figures_thesis", "eval_kde", "eval_kde_runs_"+str(num_runs)+".pdf")
 plt.savefig(path_cur_plot, format="pdf")
 
 plt.show()
--- recorded_data/eval_2_sinus/plot_deviation_bc.py
+++ recorded_data/eval_2_sinus/plot_deviation_bc.py
@@ -14,63 +14,61 @@ name_dataset = "eval_2_sinus"
 len_traj = 450
 
 current_wd = os.getcwd()
-path_dataset = os.path.join(current_wd,"recorded_data",name_dataset)
+path_dataset = os.path.join(current_wd, "recorded_data", name_dataset)
 
 plt.rc('font', size=8)
 # plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-without_serif= font_manager.FontProperties(family='sans-serif',
-                                   style='normal', size=7.5)
+without_serif = font_manager.FontProperties(family='sans-serif',
+                                            style='normal', size=7.5)
 
 cm = 1/2.54  # centimeters in inches
 my_figsize = (15*cm, 4*cm)
 
 
-
-
-
-
 # Load all measured x values and Bhattacharrya coefficients
 x_measured = np.zeros(len_traj)
 b_coeff = np.zeros(len_traj)
-for k in range(0,len_traj):
-        path_measured_state = os.path.join(path_dataset,"mpc","measured_states","real_state_k_"+str(k)+".npy")
-        x_measured[k] = np.load(path_measured_state)[0]
+for k in range(0, len_traj):
+    path_measured_state = os.path.join(
+        path_dataset, "mpc", "measured_states", "real_state_k_"+str(k)+".npy")
+    x_measured[k] = np.load(path_measured_state)[0]
 
-        path_b_coeff = os.path.join(path_dataset,"disturbance_estimation","bhattacharyya_coefficients","b_coeff_k_"+str(k)+".npy")
-        b_coeff[k] = np.load(path_b_coeff)[0]
+    path_b_coeff = os.path.join(path_dataset, "disturbance_estimation",
+                                "bhattacharyya_coefficients", "b_coeff_k_"+str(k)+".npy")
+    b_coeff[k] = np.load(path_b_coeff)[0]
 
 # Load reference trajectory
-path_ref_traj = os.path.join(path_dataset,"ref_traj_sinus.csv")
-ref_traj = np.genfromtxt(path_ref_traj, delimiter=',')[0,0:450]
+path_ref_traj = os.path.join(path_dataset, "ref_traj_sinus.csv")
+ref_traj = np.genfromtxt(path_ref_traj, delimiter=',')[0, 0:450]
 
-k = list(range(0,len_traj))
+k = list(range(0, len_traj))
 
 # Plot state deviations
-fig = plt.figure(1,figsize=my_figsize)
+fig = plt.figure(1, figsize=my_figsize)
 fig.tight_layout()
 
-plt.ylabel("${x}_{\mathrm{m},k}-{x}_{\mathrm{ref},k}$",usetex=True)
-plt.xlabel("Timestep $k$",usetex=True)
+plt.ylabel("${x}_{\mathrm{m},k}-{x}_{\mathrm{ref},k}$", usetex=True)
+plt.xlabel("Timestep $k$", usetex=True)
 
-plt.plot(k,-(ref_traj-x_measured),lw=0.8)
+plt.plot(k, -(ref_traj-x_measured), lw=0.8)
 
 plt.tight_layout()
-path_cur_plot = os.path.join(path_dataset,name_dataset+"_plot_deviations.pdf")
+path_cur_plot = os.path.join(path_dataset, name_dataset+"_plot_deviations.pdf")
 plt.savefig(path_cur_plot, format="pdf", bbox_inches="tight")
 
 # Plot Bhatt. coeff.
-fig = plt.figure(2,figsize=my_figsize)
+fig = plt.figure(2, figsize=my_figsize)
 fig.tight_layout()
 
-plt.ylabel("Bhatt. coeff. $b_c$",usetex=True)
-plt.xlabel("Timestep $k$",usetex=True)
+plt.ylabel("Bhatt. coeff. $b_c$", usetex=True)
+plt.xlabel("Timestep $k$", usetex=True)
 
-plt.plot(k,b_coeff,lw=0.8)
+plt.plot(k, b_coeff, lw=0.8)
 
 plt.tight_layout()
-path_cur_plot = os.path.join(path_dataset,name_dataset+"_b_c.pdf")
+path_cur_plot = os.path.join(path_dataset, name_dataset+"_b_c.pdf")
 plt.savefig(path_cur_plot, format="pdf", bbox_inches="tight")
 
 plt.show()
